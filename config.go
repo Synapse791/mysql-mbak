e13c05d0c2a863d40d2775eff2e7731bb073905f
Iain Earl <iain@blurgroup.com>
2015-06-19 14:37:25 +0100

    Added config structs

---
+++ config.go
@@ -0,0 +1,22 @@
+package main
+
+import ()
+
+type Config struct {
+	Connections []ConnectionConfig
+	S3Config	S3Config
+}
+
+type ConnectionConfig struct {
+	S3Bucket    string      `json:"s3_upload"`
+	Hostname    string		`json:"hostname"`
+	Username    string      `json:"username"`
+	Password    string      `json:"password"`
+	Databases   []string	`json:"databases"`
+}
+
+type S3Config struct {
+	Region          string	`json:"region"`
+	AccessKey       string  `json:"access_key"`
+	ClientSecret    string  `json:"client_secret"`
+}

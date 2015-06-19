ab5ceb4284eba4a9bc63e56c9b70856252f38650
Iain Earl <iain@blurgroup.com>
2015-06-19 14:45:19 +0100

    Added verbose flag

--- mysql-mbak.go
+++ mysql-mbak.go
@@ -1,7 +1,16 @@
 package main
 
-import ()
+import (
+	"flag"
+)
+
+var verbose bool
+
+func init() {
+	flag.BoolVar(&verbose, "v", false, "enable verbose logging")
+	flag.BoolVar(&verbose, "verbose", false, "enable verbose logging")
+}
 
 func main() {
-
+	flag.Parse()
 }

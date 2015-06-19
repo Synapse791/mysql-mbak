e3cbc1529bca36013c1a55720e93e8eb45275a04
Iain Earl <iain@blurgroup.com>
2015-06-19 14:56:50 +0100

    Added initial help text

--- mysql-mbak.go
+++ mysql-mbak.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"flag"
+    "flag"
+    "fmt"
+    "os"
 )
 
-var verbose bool
+var (
+    verbose     bool
+    showHelp    bool
+)
 
 func init() {
-	flag.BoolVar(&verbose, "v", false, "enable verbose logging")
-	flag.BoolVar(&verbose, "verbose", false, "enable verbose logging")
+    flag.BoolVar(&verbose, "v", false, "enable verbose logging")
+    flag.BoolVar(&verbose, "verbose", false, "enable verbose logging")
+
+    flag.BoolVar(&showHelp, "h", false, "print usage information")
+    flag.BoolVar(&showHelp, "help", false, "print usage information")
 }
 
 func main() {
-	flag.Parse()
+    flag.Parse()
+
+    if showHelp {
+        Usage()
+    }
+}
+
+func Usage() {
+    fmt.Println("MySQL mBak")
+    fmt.Println("  Backup multiple MySQL hosts and Databases from one place\n")
+    fmt.Println("Usage:")
+    flag.PrintDefaults()
+    os.Exit(0)
 }

a65457673d6250d3aea28cfcf342051690fb315b
Iain Earl <iain@blurgroup.com>
2015-06-22 10:04:39 +0100

    Adding debug lines

--- config.go
+++ config.go
@@ -42,15 +42,18 @@ func SetConfig(config *Config) error {
 func ReadHostsConfig(config *Config) error {
     hostsFile   := fmt.Sprintf("%s/hosts.json", CONF_DIR)
 
+    logger.Debug("checking file %s exists", hostsFile)
     if _, err := os.Stat(hostsFile); err != nil {
         return fmt.Errorf("config file %s not found", hostsFile)
     }
 
+    logger.Debug("reading file %s", hostsFile)
     rawHosts, readErr := ioutil.ReadFile(hostsFile)
     if readErr != nil {
         return fmt.Errorf("failed to read config file %s", hostsFile)
     }
 
+    logger.Debug("decoding JSON from %s", hostsFile)
     jsonErr := json.Unmarshal(rawHosts, &config.Connections)
     if jsonErr != nil {
         return fmt.Errorf("invalid json in file %s", hostsFile)
@@ -63,23 +66,31 @@ func ReadS3Config(config *Config) error {
     s3File := fmt.Sprintf("%s/s3.json", CONF_DIR)
     s3Check := false
 
+    logger.Debug("checking if s3 config is required")
     for _, conn := range config.Connections {
         if conn.S3Bucket != "" {
+            logger.Debug("s3 config required for %s", conn.Hostname)
             s3Check = true
         }
     }
 
-    if s3Check == false { return nil }
+    if s3Check == false {
+        logger.Debug("s3 config not required. No buckets specified")
+        return nil
+    }
 
+    logger.Debug("checking file %s exists", s3File)
     if _, err := os.Stat(s3File); os.IsNotExist(err) {
         return fmt.Errorf("s3_bucket set but %s config file not found", s3File)
     }
 
+    logger.Debug("reading file %s", s3File)
     rawS3, readErr := ioutil.ReadFile(s3File)
     if readErr != nil {
         return fmt.Errorf("failed to read config file %s", s3File)
     }
 
+    logger.Debug("decoding JSON from %s", s3File)
     jsonErr := json.Unmarshal(rawS3, &config.S3Config)
     if jsonErr != nil {
         return fmt.Errorf("invalid json in file %s", s3File)
